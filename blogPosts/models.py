4d551ab466f4c5b321ef829f2afa1925234b3ab7
Olly Parry-Evans <o.parryevans@gmail.com>
2019-01-14 16:31:22 +0000

    Adding BlogPost Class and creating admin accounts

--- blogPosts/models.py
+++ blogPosts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    post_title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.post_title
