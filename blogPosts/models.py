bea70b61741ec7472bce75fb8a808367f53aab50
callumblanchard <callum.blanchard@hotmail.co.uk>
2019-01-15 23:02:06 +0000

    altered db fields

--- blogPosts/models.py
+++ blogPosts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class BlogPost(models.Model):
     post_title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=400, default="Subtitle")
-    pub_date = models.DateTimeField('Published ')
-    author = models.CharField(max_length=25, default="Author")
+    pub_date = models.DateTimeField('Published')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField(default="Body")
 
     def __str__(self):
